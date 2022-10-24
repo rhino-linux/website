9660375a2356c56530fc5349fd9f13b5a7f35d84
Chadano <airriflepellet@gmail.com>
2022-10-24 17:23:44 +0100

    chore: initial commit

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+function ddshow() {
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("site").style.display = "none";
+}
+
+// Hide dropdown menu
+function ddhide() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("site").style.display = "block";
+}
