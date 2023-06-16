ed433c5b009d22e4790f03def9cfe0d61b530a36
ajstrongdev <adamjsalt@icloud.com>
2023-06-16 22:49:55 +0100

    edit(*): Add app banner
    Add app banner which toggles information

---
+++ assets/js/ecosystem.js
@@ -0,0 +1,9 @@
+function appshide() {
+    document.getElementById("ecosystem-main").style.display = "block";
+    document.getElementById("ecosystem-wizard").style.display = "none";
+}
+
+function wizardshow() {
+    document.getElementById("ecosystem-main").style.display = "none";
+    document.getElementById("ecosystem-wizard").style.display = "block";
+}
