0278b6821deff3971ee2849263c7b695307a5e32
ajstrongdev <adamjsalt@icloud.com>
2023-08-02 17:32:47 +0100

    add: mobile menu
    Still need to add the header changes to all pages.#

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function ddshow() {
+    document.getElementById("dropdown").style.display = "block";
+}
+
+// Hide dropdown menu
+function ddhide() {
+    document.getElementById("dropdown").style.display = "none";
+}
