6808f5f9593d3bc1faaaf7c4b3d0a38665b439cb
ook37 <oren+12345@taumoda.com>
2023-08-03 14:53:47 -0400

    patch buttons properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,18 @@ module.exports = {
       },
       dropShadow: {
         'header-drop': '2px 4px 10px rgba(40, 30, 45, 1)',
-      }
+      },
+    },
+    fontSize: {
+      xs: ['0.75rem', {lineHeight: '1rem',}],
+      sm: ['0.875rem', {lineHeight: '1.25rem',}],
+      base: ['1rem', {lineHeight: '1.5rem',}],
+      btn: ['1.15rem', {lineHeight: '1.6rem',}],
+      lg: ['1.125rem', {lineHeight: '1.75rem',}],
+      xl: ['1.25rem', {lineHeight: '1.75rem',}],
+      '2xl': ['1.5rem', {lineHeight: '2rem',}],
+      '3xl': ['1.875rem', {lineHeight: '2.25rem',}],
+      '4xl': ['2.25rem', {lineHeight: '2.5rem',}],
     },
   },
   plugins: [],
