4e8132e74f138304c8de9860c15e4f2cc35e0ea1
Andrew M McCall <andrew.m.mccall@gmail.com>
2023-09-09 20:14:22 -0400

    refactor hamburger menue for better accessibility

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,21 @@
-function ddshow() {
-    document.getElementById("dropdown").style.display = "block";
-}
 
-// Hide dropdown menu
-function ddhide() {
-    document.getElementById("dropdown").style.display = "none";
-}
+    
+
+    function ddshow() {
+        const dropdown = document.getElementById("dropdown") || null
+        if (!dropdown) return null
+        dropdown.style.display = "block";
+        dropdown.setAttribute("aria-expanded", true)
+    
+        
+    }
+    
+    // Hide dropdown menu
+    function ddhide() {
+        const dropdown = document.getElementById("dropdown") || null
+        if (!dropdown) return null
+        dropdown.style.display = "none";
+        dropdown.setAttribute("aria-expanded", false)
+    }
+
+
