36b3947b48785b95438eeaa26476b307972f1e0d
ook37 <oren+12345@taumoda.com>
2024-09-29 22:15:46 -0400

    quote & spacing

--- src/components/index/quote.jsx
+++ src/components/index/quote.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 const quoteText = [
     {
+        quote: "One of the best rolling release distros out there.",
+        author: "Sourav Rudra",
+        link: "https://news.itsfoss.com/rhino-linux-2024-2-release/",
+        source: "It's FOSS" 
+    },
+    {
         quote: "Rhino Linux's latest release makes it the Swiss Army knife of Linux distributions.",
         author: "Jack Wallen",
         link: "https://www.zdnet.com/article/rhino-linux-latest-release-makes-it-the-swiss-army-knife-of-linux-distributions/",
@@ -11,7 +17,7 @@ const quoteText = [
 
 function Quote() {
     return (
-        <div className="md:w-[85%] m-auto grid grid-cols-1 gap-4">
+        <div className="md:w-[85%] m-auto grid grid-cols-1 gap-4 pb-8">
             {quoteText.map((item, index) => (
                 <blockquote key={index} className="text-xl text-white text-center m-auto px-8 py-4">
                     <p className="italic">&quot;{item.quote}&quot;</p>
