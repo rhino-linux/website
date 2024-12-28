070978213d7383d42e030d96897d5ae6d3888d4a
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-12-28 17:22:39 -0500

    fix ub2r redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/ub2r',
+        destination: 'https://raw.githubusercontent.com/rhino-linux/os/refs/heads/main/ub2r.sh',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
