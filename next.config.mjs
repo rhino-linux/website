f7dbebef195972412ed98342e3926446d87d8f56
ook37 <oren+12345@taumoda.com>
2025-01-07 17:31:16 -0500

    add push action

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   async redirects() {
     return [
