f7c0c085148559f6e6c913b4675e772a3a84d3b2
ajstrongdev <ajstrongdev@proton.me>
2025-07-14 21:35:06 +0100

    fix: mobile alignment goof

--- src/components/index/sponsorship.jsx
+++ src/components/index/sponsorship.jsx
@@ -12,7 +12,7 @@ const sponsorships = [
 function Sponsorship() {
     return (
         <div className="md:w-[85%] m-auto grid grid-cols-1 gap-4 pb-4 md:pt-4">
-            <h2 className="md:text-center text-3xl font-bold px-2 text-rhino-purple">Our Sponsors</h2>
+            <h2 className="text-center text-3xl font-bold px-2 text-rhino-purple">Our Sponsors</h2>
             <div className="flex items-center justify-center flex-wrap gap-4">
                 {sponsorships.map((sponsor, index) => (
                     <a key={index} href={sponsor.link} target="_blank" rel="noopener noreferrer" className="p-4">
