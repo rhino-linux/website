db1b20815e27e4b21a654ffcedccf28926270489
ajstrongdev <ajstrongdev@proton.me>
2026-02-14 21:30:15 +0000

    fix: remove additional announcement

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Link from "next/link";
 import Hero from "../components/index/hero";
 import Description from "../components/index/description";
 import Content from "../components/index/content";
 import Ecosystem from "../components/index/ecosystem";
 import Quote from "../components/index/quote";
-import Announcement from "../components/index/announcement";
+// import Announcement from "../components/index/announcement";
 import BugAnnouncement from "../components/index/bug-announcement";
 import Sponsorship from "../components/index/sponsorship";
 import Footer from "../components/main/footer";
@@ -14,9 +13,12 @@ import SiteData from "../components/main/sitedata";
 export default function Home() {
   return (
     <main>
-      <SiteData title="Homepage" description="An Ubuntu-based, rolling release distribution, with Pacstall & XFCE at its core." />
+      <SiteData
+        title="Homepage"
+        description="An Ubuntu-based, rolling release distribution, with Pacstall & XFCE at its core."
+      />
       <Hero />
-      <Announcement />
+      {/* <Announcement /> */}
       <BugAnnouncement />
       <Description />
       <Ecosystem />
